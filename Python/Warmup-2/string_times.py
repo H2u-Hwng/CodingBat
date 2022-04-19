36fb33cd23f1416eb38d2a0015e6fffc59669c3e
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-19 16:51:24 -0700

    Create string_times.py

---
+++ Python/Warmup-2/string_times.py
@@ -0,0 +1,5 @@
+# problem: https://codingbat.com/prob/p193507
+# Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+def string_times(str, n):
+  return str*n
