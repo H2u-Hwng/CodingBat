0b4610f7673f77dc830f17607346e5ec281225ea
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-21 23:26:44 -0700

    Create string_splosion.py

---
+++ Python/Warmup-2/string_splosion.py
@@ -0,0 +1,10 @@
+# problem: https://codingbat.com/prob/p118366
+# Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+def string_splosion(str):
+  
+  result = ''
+  for i in range(len(str)):
+    result += str[:i + 1]
+  
+  return result
