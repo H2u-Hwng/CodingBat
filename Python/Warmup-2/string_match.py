f051ecb58f4fa03f053ad3761697a91e807134b7
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-23 17:11:27 -0700

    Create string_match.py

---
+++ Python/Warmup-2/string_match.py
@@ -0,0 +1,17 @@
+# problem: https://codingbat.com/prob/p182414
+# Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
+
+def string_match(a, b):
+  # Figure which string is shorter.
+  shorter = min(len(a), len(b))
+  count = 0
+  
+  # Loop i over every substring starting spot.
+  # Use length-1 here, so can use char str[i+1] in the loop
+  for i in range(shorter-1):
+    a_sub = a[i:i+2]
+    b_sub = b[i:i+2]
+    if a_sub == b_sub:
+      count = count + 1
+
+  return count
