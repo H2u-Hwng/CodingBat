9f0d966ba9b174ad080e1e546d0048c804a5a2a0
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-24 16:02:33 -0700

    Create hello_name.py

---
+++ Python/String-1/hello_name.py
@@ -0,0 +1,5 @@
+# problem: https://codingbat.com/prob/p115413
+# Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+def hello_name(name):
+  return 'Hello ' + name + '!' 
