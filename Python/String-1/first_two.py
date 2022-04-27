a186ee0829914b07dd7c59c1089ac23c6f7ffe7d
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-27 08:09:03 -0700

    Create first_two.py

---
+++ Python/String-1/first_two.py
@@ -0,0 +1,6 @@
+# problem: https://codingbat.com/prob/p184816
+# Given a string, return the string made of its first two chars.
+# If the string is shorter than length 2, return whatever there is.
+
+def first_two(str):
+  return str[:2]
