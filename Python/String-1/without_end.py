1dbd2046623c1d1e546f09c0f9d39405734c3c55
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-28 17:13:21 -0700

    Create without_end.py

---
+++ Python/String-1/without_end.py
@@ -0,0 +1,6 @@
+# problem: https://codingbat.com/prob/p138533
+# Given a string, return a version without the first and last char.
+# The string length will be at least 2.
+
+def without_end(str):
+  return str[1:-1]
