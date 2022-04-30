54c268b8df6ea9ecf6d4e720ab62042a56152873
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-29 19:46:12 -0700

    Create count_hi.py

---
+++ Python/String-2/count_hi.py
@@ -0,0 +1,5 @@
+# problem: https://codingbat.com/prob/p167246
+# Return the number of times that the string "hi" appears anywhere in the given string.
+
+def count_hi(str):
+  return str.count('hi')
