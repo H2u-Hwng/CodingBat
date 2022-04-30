6d1ae44d5b7af4c934c2b4b6365cd21a96545502
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-29 19:47:36 -0700

    Create cat_dog.py

---
+++ Python/String-2/cat_dog.py
@@ -0,0 +1,5 @@
+# problem: https://codingbat.com/prob/p164876
+# Return True if the string "cat" and "dog" appear the same number of times in the given string.
+
+def cat_dog(str):
+  return str.count('cat') == str.count('dog')
