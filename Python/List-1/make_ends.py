3c3f23ba76210463bd7f9e23562e25835a6f8df8
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-05-04 01:02:20 -0700

    Create make_ends.py

---
+++ Python/List-1/make_ends.py
@@ -0,0 +1,5 @@
+# problem: https://codingbat.com/prob/p124806
+# Given an array of ints, return a new array length 2 containing the first and last elements from the original array.
+
+def make_ends(nums):
+  return [nums[0], nums[-1]]
