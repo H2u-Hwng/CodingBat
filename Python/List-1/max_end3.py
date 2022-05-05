860b3df6e339a74a78aa507e95df04a0e3692880
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-05-05 11:25:14 -0700

    Create max_end3.py

---
+++ Python/List-1/max_end3.py
@@ -0,0 +1,8 @@
+# problem: https://codingbat.com/prob/p135290
+# Given an array of ints length 3, 
+# figure out which is larger, the first or last element in the array, and set all the other elements to be that value. 
+# Return the changed array.
+
+def max_end3(nums):
+  n = max(nums[0], nums[-1])
+  return [n, n, n]
