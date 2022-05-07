4f0fac10c9a859672b7fa57ce0636202df034fe7
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-05-07 15:29:31 -0700

    Create reverse3.py

---
+++ Python/List-1/reverse3.py
@@ -0,0 +1,5 @@
+# problem: https://codingbat.com/prob/p192962
+# Given an array of ints length 3, return a new array with the elements in reverse order.
+
+def reverse3(nums):
+  return nums[-1::-1]
