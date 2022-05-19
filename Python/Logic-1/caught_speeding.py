856cd76b351485da7c5167e80a3fb0bebd5dad5d
Huu Hung Nguyen <79944868+H2u-Hwng@users.noreply.github.com>
2022-05-18 22:19:45 -0700

    Create caught_speeding.py

---
+++ Python/Logic-1/caught_speeding.py
@@ -0,0 +1,15 @@
+# problem: https://codingbat.com/prob/p137202
+# Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket.
+# If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. else, the result is 2.
+# Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+
+def caught_speeding(speed, is_birthday):
+  if is_birthday:
+    speed -= 5
+  
+  if speed <= 60: 
+    return 0
+  elif speed <= 80:
+    return 1
+  else:
+    return 2
