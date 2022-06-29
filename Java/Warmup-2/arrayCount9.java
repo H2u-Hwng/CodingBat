1a8be5adb4291ec61ddfd233f9795ce4e632375f
Huu Hung Nguyen <79944868+hhnguyen2000@users.noreply.github.com>
2022-06-28 23:48:27 -0700

    Create arrayCount9.java

---
+++ Java/Warmup-2/arrayCount9.java
@@ -0,0 +1,16 @@
+/*
+ * problem: https://codingbat.com/prob/p184031
+ * Given an array of ints, return the number of 9's in the array.
+ */
+
+public int arrayCount9(int[] nums) {
+  int count = 0;
+  
+  for (int n: nums) {
+    if (n == 9) {
+      count++;
+    }
+  }
+  
+  return count;
+}
