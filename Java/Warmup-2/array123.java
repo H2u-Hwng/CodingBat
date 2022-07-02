391c62d11f351d2d8f795e06ca5391ef2b676d5d
Huu Hung Nguyen <79944868+hhnguyen2000@users.noreply.github.com>
2022-07-01 20:23:22 -0700

    Create array123.java

---
+++ Java/Warmup-2/array123.java
@@ -0,0 +1,14 @@
+/*
+ * problem: https://codingbat.com/prob/p136041
+ * return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+ */
+
+public boolean array123(int[] nums) {
+  for (int i = 0; i < nums.length - 2; i++) {
+    if (nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) {
+      return true;
+    }
+  }
+  
+  return false;
+}
