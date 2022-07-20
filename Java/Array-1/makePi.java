82f686fc7ca06936596ed45aac4ae1fb4d0a79c5
Huu Hung Nguyen <79944868+hhnguyen2000@users.noreply.github.com>
2022-07-19 21:22:05 -0700

    Create makePi.java

---
+++ Java/Array-1/makePi.java
@@ -0,0 +1,10 @@
+/*
+ * problem: https://codingbat.com/prob/p167011
+ * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+ */
+
+public int[] makePi() {
+  int[] result = {3, 1, 4};
+  
+  return result;
+}
