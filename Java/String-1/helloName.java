de681502598db9129248f6d7cc9bd17ff94ed476
Huu Hung Nguyen <79944868+hhnguyen2000@users.noreply.github.com>
2022-07-29 23:44:19 -0700

    Create helloName.java

---
+++ Java/String-1/helloName.java
@@ -0,0 +1,8 @@
+/*
+ * problem: https://codingbat.com/prob/p171896
+ * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+ */
+
+public String helloName(String name) {
+  return "Hello " + name + "!";
+}
