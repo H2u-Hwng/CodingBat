c0166b930981076e937cd811a04251cc4009a5cd
Huu Hung Nguyen <79944868+hhnguyen2000@users.noreply.github.com>
2022-08-07 22:44:42 -0700

    Create firstHalf.java

---
+++ Java/String-1/firstHalf.java
@@ -0,0 +1,8 @@
+/*
+ * problem: https://codingbat.com/prob/p172267
+ * Given a string of even length, return the first half. 
+ */
+
+public String firstHalf(String str) {
+  return str.substring(0, str.length() / 2);
+}
